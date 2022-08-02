4b1bd080f13bf4f8905fcaed8f1bf02ee3ab00c5
Edward Hong <edward.hong527@gmail.com>
2022-08-03 00:13:58 +1200

    S03L07 Keep Your Tests Simple!

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -4,15 +4,15 @@ import { add } from './math'
 
 it('should summarize all number values in an array', () => {
   // Arrange
-  const numbers = [1, 2, 3]
-  const expectedResult = numbers.reduce(
-    (prevValue, curValue) => prevValue + curValue,
-    0
-  )
+  const numbers = [1, 2]
 
   // Act
   const result = add(numbers)
 
   // Assert
+  const expectedResult = numbers.reduce(
+    (prevValue, curValue) => prevValue + curValue,
+    0
+  )
   expect(result).toBe(expectedResult)
 })
